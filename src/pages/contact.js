8e3cf10da8deba84ef620b5891442a507578c43f
Selim23 <60688990+Selim23@users.noreply.github.com>
2020-11-12 02:40:36 +0100

    kleine aanpassing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from "gatsby"
-import {RiMailSendFill,RiPhoneLine,RiUserLocationLine} from 'react-icons/ri'
+import { RiMailSendFill, RiPhoneLine, RiUserLocationLine } from 'react-icons/ri'
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
@@ -68,20 +68,20 @@ const Contact = () => {
                 </div>
                 <div className="contact-info">
                     <div>
-                        <RiMailSendFill style = {{height: '4rem', width:'4rem'}} />
-                        <p>Send us an email at{" "} 
-                        <a target="__blank" href={`mailto:${contactPageEmail}`}>{contactPageEmail}</a>
+                        <RiMailSendFill style={{ height: '4rem', width: '4rem' }} />
+                        <p>Send us an email at{" "}
+                            <a target="__blank" href={`mailto:${contactPageEmail}`}>{contactPageEmail}</a>
                         </p>
                     </div>
                     <div>
-                    <RiPhoneLine style = {{height: '4rem', width:'4rem'}} />
+                        <RiPhoneLine style={{ height: '4rem', width: '4rem' }} />
                         <p>Call us:{" "} {contactPagePhone} </p>
                     </div>
                     <div>
-                    <RiUserLocationLine style = {{height: '4rem', width:'4rem'}} />
+                        <RiUserLocationLine style={{ height: '4rem', width: '4rem' }} />
                         <p>{contactPageAddress},{" "}{contactPagePostcode} {contactPageCity} </p>
                     </div>
-                    
+
 
                 </div>
             </Wrapper>
